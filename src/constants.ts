9e9b9dfe87d8a6f36bfefa850a2e36381b3e1476
waki285 <suzuneu@suzuneu.com>
2023-10-31 19:35:10 +0900

    bug fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SERVICE_NAME = "File Converter";
-export const FFMPEG_CORE_VERSION = "0.10.0";
-export const CORE_PATH = `https://unpkg.com/@ffmpeg/core@${FFMPEG_CORE_VERSION}/dist/ffmpeg-core.js`;
-export const WASM_PATH = `https://unpkg.com/@ffmpeg/core@${FFMPEG_CORE_VERSION}/dist/ffmpeg-core.wasm`;
+export const FFMPEG_CORE_VERSION = "0.12.4";
+export const CORE_PATH = `https://unpkg.com/@ffmpeg/core@${FFMPEG_CORE_VERSION}/dist/umd/ffmpeg-core.js`;
+export const WASM_PATH = `https://unpkg.com/@ffmpeg/core@${FFMPEG_CORE_VERSION}/dist/umd/ffmpeg-core.wasm`;
